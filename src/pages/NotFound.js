160011a10414bfe1cdf1f6dc596642dec0e988cd
WalterCosta <156-201707140059@users.noreply.gitlab.lasse.ufpa.br>
2021-11-17 23:32:03 -0300

    Feito Requisito 1

---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NotFund extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <h2>Not Found</h2>
+      </div>
+    );
+  }
+}
+
+export default NotFund;
